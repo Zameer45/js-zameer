b97adef1928ae28a0432d5f16d3dab00d2ba7034
Zameer45 <118253201+Zameer45@users.noreply.github.com>
2024-11-21 14:05:58 +0000

    conversion

---
+++ basics/conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "zameer"
+
+
+
+//console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+// "" => false
+// "zameer" => true
+
+let isloggedIn = "33"
+
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
