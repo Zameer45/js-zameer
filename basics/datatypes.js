23fc12086d0dac117b58ad08b4ca46b00a36b943
Zameer45 <118253201+Zameer45@users.noreply.github.com>
2024-11-21 13:00:01 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert("3+3") we re using nodejs, not brower
+
+console.log(3 + 3); // code redeability is high 
+
+console.log("zameer"); 
+
+let name ="raj" //string datatype
+let age = 18
+let isloggedIn =false
+let state;
+
+//number => 2 to power 53
+//bignit
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined 
+
+
--- basics/variables.js
+++ basics/variables.js
@@ -2,10 +2,12 @@ const accountId = 114455
 let accountemail = "zameer@gmail.com"
 var accountpassword = "123"
 accountcity = "pune"
+let accountState;
+
 
 accountemail= "zameer45@.com"
 accountpassword ="0000"
 accountcity ="mumbai"
 
 console.log(accountId);
-console.table([accountId,accountemail,accountpassword,accountcity])
+console.table([accountId,accountemail,accountpassword,accountcity,accountState])
